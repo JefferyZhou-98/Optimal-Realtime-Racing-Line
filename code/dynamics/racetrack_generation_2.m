781fd6611b1ae0412ea28c222eadf7b9065d308b
JefferyZhou-98 <jefferyzh98@gmail.com>
2023-04-04 23:31:30 -0400

    cleaned up the files and all code is new

---
+++ code/dynamics/racetrack_generation_2.m
@@ -0,0 +1,53 @@
+function [x,y] = racetrack_generation_2()
+x1 = flip(0:20);
+y1 = -(20^2 - (x1-20).^2).^0.5;
+
+x2 = (0:20);
+y2 = (20^2 - (x2-20).^2).^0.5;
+
+x3 = (20:50);
+y3 = zeros(1,31) + 20;
+
+x4 = (50:60);
+y4 = -(10^2 - (x4-50).^2).^0.5 + 30;
+
+x5 = (60:70);
+y5 = (10^2 - (x5-70).^2).^0.5 + 30;
+
+x6 = (70:110);
+y6 = zeros(1,length(x6)) + 40;
+
+x7 = (110:130);
+y7 = (20^2 - (x7-110).^2).^0.5 + 20;
+
+x8 = zeros(1,81) + 130;
+y8 = flip(-60:20);
+
+x9 = flip(110:130);
+y9 = -(20^2 - (x9-110).^2).^0.5 - 60; 
+
+x10 = flip(80:110);
+y10 = zeros(1,31) - 80;
+
+x11 = flip(70:80);
+y11 = -(10^2 - (x11-80).^2).^0.5 - 70;
+
+x12 = zeros(1,41) + 70;
+y12 = (-70:-30);
+
+x13 = flip(60:70);
+y13 = (10^2 - (x13-60).^2).^0.5 - 30;
+
+x14 = flip(20:60);
+y14 = zeros(1,41) - 20;
+
+
+x_save = [x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14];
+y_save = [y1,y2,y3,y4,y5,y6,y7,y8,y9,y10,y11,y12,y13,y14];
+
+% applying coordinate transformation to shift the plot
+M = [cos(pi/4) -sin(pi/4); sin(pi/4) cos(pi/4)];
+transformed_track = M*[x_save; y_save];
+
+x = transformed_track(1,:); y = transformed_track(2,:);
+end    
--- code/dynamics/racing_sim_new.m
+++ code/dynamics/racing_sim_new.m
@@ -24,8 +24,8 @@ g = 9.81;
 
 % initial conditions ------------------------------------------------------
 % states
-x_0 = 0; 
-y_0 = 0;
+x_0 = 20; 
+y_0 = -20;
 theta_0 = pi; theta_k = theta_0;
 phi_r_0 = 0;
 phi_l_0 = 0;
@@ -34,8 +34,8 @@ vx_0 = 0; Vx_k = vx_0;
 vy_0 = 0; Vy_k = vy_0;
 theta_d_0 = 0; theta_d_k = theta_d_0;
 % inputs
-T_L_0 = 2000; % Nm
-T_R_0 = 2030; % Nm
+T_L_0 = 20; % Nm
+T_R_0 = 23; % Nm
 % slipping condition
 k_slip = 1;
 
@@ -45,28 +45,28 @@ x_save = ones(1,10); x_save(1) = x_0;
 y_save = ones(1,10); y_save(1) = y_0;
 theta_save = ones(1,10); theta_save(1) = theta_0;
 
-t = linspace(0, 5, 10);
-dt = t(2) - t(1);
-for i = 1:length(t) - 1
-    x_init = [x_save(i); y_save(i); theta_save(i)];
-    u_init = [T_L_0; T_R_0];
-    s_dd = racing_ode_new(x_init, u_init, k_slip);
-    % grabbing the second order terms
-    speed_k = s_dd(1); acc = s_dd(2); theta_dot_k = s_dd(3); alpha_L = s_dd(4); alpha_R = s_dd(5);
-    % propagation
-    x_save(i+1) = x_save(i) + (speed_k*dt + 0.5*acc*(dt^2))*cos(theta_k);
-    y_save(i+1) = y_save(i) + (speed_k*dt + 0.5*acc*(dt^2))*sin(theta_k);
-    % update    
-    theta_k = theta_k + theta_dot_k*dt;
-
-    % storing
-end
+% t = linspace(0, 5, 10);
+% dt = t(2) - t(1);
+% for i = 1:length(t) - 1
+%     x_init = [x_save(i); y_save(i); theta_save(i)];
+%     u_init = [T_L_0; T_R_0];
+%     s_dd = racing_ode_new(x_init, u_init, k_slip);
+%     % grabbing the second order terms
+%     speed_k = s_dd(1); acc = s_dd(2); theta_dot_k = s_dd(3); alpha_L = s_dd(4); alpha_R = s_dd(5);
+%     % propagation
+%     x_save(i+1) = x_save(i) + (speed_k*dt + 0.5*acc*(dt^2))*cos(theta_k);
+%     y_save(i+1) = y_save(i) + (speed_k*dt + 0.5*acc*(dt^2))*sin(theta_k);
+%     % update    
+%     theta_k = theta_k + theta_dot_k*dt;
+% 
+%     % storing
+% end
 
 % waypoint assignment -----------------------------------------------------
 x_waypoints = ones(1, 20);
 y_waypoints = ones(1, 20);
 % importing track
-[x_track, y_track] = track();
+[x_track, y_track] = racetrack_generation_2();
 figure(1)
 plot(x_track,y_track, linewidth=4, Color='k')
 axis equal
@@ -76,12 +76,13 @@ for i = 1:length(x_track) - 20
     if rem(i, 20) == 0 || i == 1
         x_waypoints(i) = x_track(i);
         y_waypoints(i) = y_track(i);
-        [x_poly, y_poly, p] = track_sensor(i, i);
+        [x_poly, y_poly, p] = track_sensor_new(i, i);
         y_plot = p(1)*x_poly.^2 + p(2)*x_poly + p(3);
         scatter(x_waypoints(i), y_waypoints(i), 'filled', LineWidth=4, MarkerFaceColor="#D95319");
         hold on
         plot(x_poly, y_plot, LineStyle=":", LineWidth=2.5);
         drawnow
+        legend("Race Track","Waypoints","Polyfit Trajectory", fontsize=15)
     end
 end
 
